eb351f780d08a43e303306bf83f0d8ee6edddb85
alanbrezman <brezmanalan1997@gmail.com>
2022-03-25 01:21:45 -0300

    upload proyect

---
+++ src/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export function Header(){
+
+    return  <header>
+                <img className="logotipo" src="/assets/logo.svg" alt="logotipo"/>
+            </header>
+}
