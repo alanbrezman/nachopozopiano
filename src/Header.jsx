180ce238c6321c844cfcd58f2822134af6a5d5c4
alanbrezman <brezmanalan1997@gmail.com>
2022-03-25 04:32:57 -0300

    Updates

--- src/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function Header(){
-
-    return  <header>
-                <img className="logotipo" src="./assets/logo.svg" alt="logotipo"/>
-            </header>
-}
