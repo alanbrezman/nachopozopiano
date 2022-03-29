23e294152acadab92cfcb394bc8592c07766f550
alanbrezman <brezmanalan1997@gmail.com>
2022-03-29 19:01:09 -0300

    .

---
+++ src/UnderConstructionPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Section } from './Section';
+import { Header } from './Header';
+import { Footer } from './Footer';
+
+export function UnderConstructionPage(){
+    
+    return (
+        <div>
+        <Header />
+        <Section />
+        <Footer />
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { UnderConstructionPage } from './UnderConstructionPage';
+import './sass/index.scss';
+
+ReactDOM.render(<UnderConstructionPage />, document.getElementById("root"));
+
