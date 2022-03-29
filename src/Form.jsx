b4c9c5f512e9f32b58162d47dc70f1c5cfa87385
alanbrezman <brezmanalan1997@gmail.com>
2022-03-29 18:10:43 -0300

    configure EmailJS

--- src/Form.jsx
+++ src/Form.jsx
@@ -8,6 +8,7 @@ export function ContactUs() {
 
     emailjs.sendForm('service_eemtb27', 'template_x5zc69h', e.target, 'W8Z0_qA8lMxjjg43Z')
       .then((result) => {
+          alert("Revisá tu casilla de correo, seguramente te llegó un mail que te he enviado ;)");
           window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
       }, (error) => {
           console.log(error.text);
